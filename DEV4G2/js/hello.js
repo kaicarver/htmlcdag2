df6e4f229979dad2178511ee33599bd82f187b2b
Kai Carver <kaicarver@gmail.com>
2023-11-13 15:15:54 +0100

    cours continue...

--- DEV4G2/js/hello.js
+++ DEV4G2/js/hello.js
@@ -5,10 +5,21 @@
 
 // alert(typeof(nbVar) + "\n" + typeof(strVar) + " " + typeof(boolOk));
 
-var prenom = prompt("Veuillez saisir votre prénom");
+//var prenom = prompt("Veuillez saisir votre prénom");
 // alert("Bonjour, " + prenom + " !");
-console.log("Bonjour, " + prenom + " !");
-document.write("Bonjour, " + prenom + " !");
+var prenom = "goo"
+// console.log("Bonjour, " + prenom + " !");
+// document.write("Bonjour, " + prenom + " !");
+// document.getElementById("demo").innerHTML = "Hello World !";
 
-document.getElementById("demo").innerHTML = "Hello World !";
+// var n = 0;
+// alert("1: " + ++n);
+// alert("2: " + n);
+// alert("3: " + n++);
+// alert("4: " + n);
 
+var texte = "Bonjour";
+alert(texte.length);
+console.log(texte.toLowerCase());
+console.log(texte.toUpperCase());
+console.log(texte.charAt(3));
