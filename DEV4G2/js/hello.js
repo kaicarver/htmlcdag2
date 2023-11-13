da35470344b0e0996d1a4a960becfe5161e085ea
Kai Carver <kaicarver@gmail.com>
2023-11-13 15:45:38 +0100

    exo avec toUpperCase()

--- DEV4G2/js/hello.js
+++ DEV4G2/js/hello.js
@@ -1,25 +1,4 @@
-// var myVar = "Ma première variable";
-// var myVar2 = "Hello world !";
 
-// var nbVar = 1, strVar = "Hello", boolOk = true;
-
-// alert(typeof(nbVar) + "\n" + typeof(strVar) + " " + typeof(boolOk));
-
-//var prenom = prompt("Veuillez saisir votre prénom");
-// alert("Bonjour, " + prenom + " !");
-var prenom = "goo"
-// console.log("Bonjour, " + prenom + " !");
-// document.write("Bonjour, " + prenom + " !");
-// document.getElementById("demo").innerHTML = "Hello World !";
-
-// var n = 0;
-// alert("1: " + ++n);
-// alert("2: " + n);
-// alert("3: " + n++);
-// alert("4: " + n);
-
-var texte = "Bonjour";
-alert(texte.length);
-console.log(texte.toLowerCase());
-console.log(texte.toUpperCase());
-console.log(texte.charAt(3));
+document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML.toUpperCase();
+var truc = prompt("Veuillez saisir qqcjose");
+alert(truc + " = " + truc.length)
