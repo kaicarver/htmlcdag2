705273f9131d9f918d05c9fc68cc75be7ef02baa
Kai Carver <kaicarver@gmail.com>
2023-11-13 12:27:45 +0100

    interaction utilisateur

--- DEV4G2/js/hello.js
+++ DEV4G2/js/hello.js
@@ -1,6 +1,14 @@
 // var myVar = "Ma première variable";
 // var myVar2 = "Hello world !";
 
-var nbVar = 1, strVar = "Hello", boolOk = true;
+// var nbVar = 1, strVar = "Hello", boolOk = true;
 
-alert(typeof(nbVar) + "\n" + typeof(strVar) + " " + typeof(boolOk));
+// alert(typeof(nbVar) + "\n" + typeof(strVar) + " " + typeof(boolOk));
+
+var boolConfirm = confirm("Je peux demander votre prénom ?");
+if (boolConfirm == true) {
+    var prenom= prompt("Veuillez saisir votre prénom");
+    alert(prenom);
+} else {
+    alert("ok salut !");
+}
