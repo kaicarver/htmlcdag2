8b4a0087684892ed33ea66d8f6b28575f0219ddb
Kai Carver <kaicarver@gmail.com>
2023-11-14 14:48:13 +0100

    exo solde meilleure solution en retournant juste la valeur

--- DEV4G2/js/solde.js
+++ DEV4G2/js/solde.js
@@ -2,10 +2,12 @@
 // Prix à payer = Prix initial x (100 - réduction) / 100
 function calculSolde(prix, remise) {
     var prixFinal = prix * (100 - remise) / 100;
-    return "prix final : " + prixFinal + " € remise : " + (prix - prixFinal) + " € <br>sur prix initial de " + prix + " € avec remise de " + remise + "%"
+    return prixFinal
   }
   
 var prix = prompt("Prix initial", 100)
 var remise = prompt("Pourcentage de remise", 10)
 
-document.getElementById("solde").innerHTML = calculSolde(prix, remise);
+prixFinal = calculSolde(prix, remise);
+
+document.getElementById("solde").innerHTML = "prix final : " + prixFinal + " € remise : " + (prix - prixFinal) + " € <br>sur prix initial de " + prix + " € avec remise de " + remise + "%"
