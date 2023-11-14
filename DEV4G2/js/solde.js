70efd0b8961e4517f3ae8e4c1cc7cea216adb79e
Kai Carver <kaicarver@gmail.com>
2023-11-14 14:29:39 +0100

    exo solde apres correction

--- DEV4G2/js/solde.js
+++ DEV4G2/js/solde.js
@@ -1,7 +1,8 @@
 
 // Prix à payer = Prix initial x (100 - réduction) / 100
 function calculSolde(prix, remise) {
-    return prix * (100 - remise) / 100
+    var prixFinal = prix * (100 - remise) / 100;
+    return "prix final : " + prixFinal + " € remise : " + (prix - prixFinal) + " € <br>sur prix initial de " + prix + " € avec remise de " + remise + "%"
   }
   
 var prix = prompt("Prix initial", 100)
