d21143eeec039feaeca1aa38ecc71eef8396cd30
Kai Carver <kaicarver@gmail.com>
2023-11-15 12:41:43 +0100

    exo JS DOM bonjour survol

--- DEV4G2/js/bonjour.js
+++ DEV4G2/js/bonjour.js
@@ -1,8 +1,13 @@
-var nom = prompt("Nom", "Toto")
-
-document.getElementById("bonjour").onclick = bonj
-
 function bonj(event) {
     console.log(event)
     event.target.innerHTML = "Bojour " + nom
 }
+function carrefunc(event) {
+    console.log(event)
+    event.target.innerHTML = "merci davoir survole le carre,  " + nom
+}
+
+var nom = prompt("Nom", "Toto")
+
+document.getElementById("bonjour").onclick = bonj
+document.getElementById("carre").onmouseover = carrefunc
