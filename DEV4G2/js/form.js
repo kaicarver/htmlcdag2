df545cc0ff5a240c8da64d6b6025e098fbbb167f
Kai Carver <kaicarver@gmail.com>
2023-11-16 10:09:52 +0100

    formulaire afficher un champ dans un autre

--- DEV4G2/js/form.js
+++ DEV4G2/js/form.js
@@ -1,8 +1,13 @@
-function controle(event) {
+/* function controle(event) {
     event.preventDefault();
     var test = document.getElementById("userInput").value;
     var test2 = document.form1.input1.value;
     alert("Vous avez tapé " + test + "=" + test2);
+} */
+
+function afficher(event) {
+    var test = document.getElementById("userInput2").value;
+    document.getElementById("userOutput2").value = test;
 }
 
-document.getElementById("controle").onclick = controle;
+document.getElementById("controle2").onclick = afficher;
