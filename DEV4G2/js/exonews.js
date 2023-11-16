aa9f7cd5b92182b76f4200ad5dc4baa7a373b3d4
Kai Carver <kaicarver@gmail.com>
2023-11-16 11:06:34 +0100

    exo radios boutons

---
+++ DEV4G2/js/exonews.js
@@ -0,0 +1,6 @@
+
+function merci(form) {
+    // on peut aussi regarder
+    //   document.form3.choix[0].checked
+    alert(" Merci : " + form.choix.value + " davoir votre email  "  + form.email.value);
+}
