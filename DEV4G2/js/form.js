e4f41013a6b23db91b0cc58b4d3ef8547726ff75
Kai Carver <kaicarver@gmail.com>
2023-11-16 10:54:33 +0100

    formulaire radios boutons

--- DEV4G2/js/form.js
+++ DEV4G2/js/form.js
@@ -5,9 +5,15 @@
     alert("Vous avez tapé " + test + "=" + test2);
 } */
 
-function afficher(event) {
+/* function afficher(event) {
     var test = document.getElementById("userInput2").value;
     document.getElementById("userOutput2").value = test;
-}
+} */
 
-document.getElementById("controle2").onclick = afficher;
+//document.getElementById("controle2").onclick = afficher;
+
+function choixprop(form) {
+    // on peut aussi regarder
+    //   document.form3.choix[0].checked
+    alert("choix : " + form.choix.value);
+}
