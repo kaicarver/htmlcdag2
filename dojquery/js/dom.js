f6e275237c9d4400886925fec25e599b7b81f049
Kai Carver <kaicarver@gmail.com>
2023-11-17 10:14:32 +0100

    cours DOM jquery

---
+++ dojquery/js/dom.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  console.log($('#paraTest').text())
+  console.log($('#paraTest').html())
+  console.log($("#inputText").val())
+  console.log($('#imgpicsum').attr('src'))
+});
