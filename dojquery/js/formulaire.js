c1c0d846d376cc3831d6782095991b31d32c199a
Kai Carver <kaicarver@gmail.com>
2023-11-17 14:39:22 +0100

    exo formulaire fait plus ou moins sans tricher

--- dojquery/js/formulaire.js
+++ dojquery/js/formulaire.js
@@ -1,15 +1,10 @@
 $(document).ready(function () {
-    var clicked = false;
     $("#soutien").click(function () {
-        if (clicked) {
-            $("#merci").text("");
-        } else {
-            $("#merci").text("Merci");
-        }
         if ($("#soutien").is(":checked")) {
             $("#merci").text("Merci bcp!");
+        } else {
+            $("#merci").text("Non merci bcp!");
         }
-        clicked =!clicked;
     })
 });
 
