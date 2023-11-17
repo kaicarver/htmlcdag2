4e76594f58440d2b0e67df886f024dbb7abe127f
Kai Carver <kaicarver@gmail.com>
2023-11-17 10:44:49 +0100

    exo DOM jquery

--- dojquery/js/dom.js
+++ dojquery/js/dom.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
   console.log($('#paraTest').html())
   console.log($("#inputText").val())
   console.log($('#imgpicsum').attr('src'))
+  $('#ecrire').text('coucou')
+  $('#ecrire').html('coucou <em>monde</em>')
+  $("#inputText").val('cou cou')
+  $("#lepara2").text("TEXT: " + $("#lepara").text() + " HTML: " + $("#lepara").html())
+
 });
