0e820c9afaa80445184819373f01b1c746289d42
Kai Carver <kaicarver@gmail.com>
2023-11-17 15:17:30 +0100

    exo formulaire light dark

--- dojquery/js/formulaire.js
+++ dojquery/js/formulaire.js
@@ -8,5 +8,20 @@ $(document).ready(function () {
             $("#merci").css("background-color", "white");
         }
     })
+    $("#dark").click(function (event) {
+        event.preventDefault();
+        $("body").css("background-color", "black");
+        $("body").css("color", "white");
+    })    
+    $("#lite").click(function (event) {
+        $("body").css("background-color", "white");
+        $("body").css("color", "black");
+    })
+    $(".button").mouseover(function (event) {
+        $(this).css("color", "red");
+    });
+    $(".button").mouseout(function (event) {
+        $(this).css("color", "black");
+    });
 });
 
