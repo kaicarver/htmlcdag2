cf3967f67ee8a98b5651490f919b46d1354db15d
Kai Carver <kaicarver@gmail.com>
2023-11-21 14:27:38 +0100

    divers jquery ui

--- dojquery/js/coursjquery.js
+++ dojquery/js/coursjquery.js
@@ -1,4 +1,33 @@
-$(document).ready(function() {
-    $("#containerAccordeon").accordion();
+$(document).ready(function () {
+    $(function () {
+        $("#draggable").draggable();
+        $("#droppable").droppable({
+            drop: function (event, ui) {
+                $(this)
+                    .addClass("ui-state-highlight")
+                    .find("p")
+                    .html("Déposé !");
+            }
+        });
+        $("#sortable").sortable();
+        $("#sortable").disableSelection();
+    });
+
+    $("#containerAccordeon").accordion({
+        animate: 750
+    });
+
     $("#acc2").accordion();
+    $("#acc2").resizable();
+
+    var listeAnimaux = [
+        "Chien",
+        "Chat",
+        "Tortue",
+        "Python",
+        "Corbeau",
+      ];
+      $( "#animaux" ).autocomplete({
+        source: listeAnimaux
+      });
 })
