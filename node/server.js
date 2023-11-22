29b45cfdc0d28eefd1045548af9315f57d9e3637
Kai Carver <kaicarver@gmail.com>
2023-11-22 12:49:44 +0100

    retourner data

--- node/server.js
+++ node/server.js
@@ -2,11 +2,15 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><h1>This is the home page</h1><a href="/students/">lien student</a> <a href="/student/">lien invalide</a></body></html>');
+        res.write('<html><body><h1>This is the home page</h1><a href="/students/">lien student</a> <a href="/student/">lien invalide</a> <a href="/data/">data</a></body></html>');
         res.end();
     } else if (req.url === '/students/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<html><body><h1>This is the student page</h1><a href="/">home</a></body></html>');
+        res.end();
+    } else if (req.url === '/data/') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({message: 'Hello world !'}));
         res.end();
     } else {
         res.end('Invalid Request !');
