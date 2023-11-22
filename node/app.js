e6cb89e48b5ab85e69abf0528a2f44e53589dedc
Kai Carver <kaicarver@gmail.com>
2023-11-22 15:42:26 +0100

    appli Express avec POST !

--- node/app.js
+++ node/app.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var path = require('path');
+
+app.get('/', function(req, res) {
+    console.log(path.resolve('index.html'));
+  res.sendFile(path.resolve('index.html'));
+});
+
+app.post('/submit-student-data', function (req, res) {
+    res.send('POST Request');
+});
 var server = app.listen(5000, function () {
     console.log('Express server listening on port '+ server.address().port);
 });
