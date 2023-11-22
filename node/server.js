539c24c7f1d0c52c4c156288da453955fab4b0bd
Kai Carver <kaicarver@gmail.com>
2023-11-22 12:42:48 +0100

    gerer URL invalide

--- node/server.js
+++ node/server.js
@@ -2,12 +2,14 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
     if (req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<html><body><h1>This is the home page</h1><a href="/students/">lien</a></body></html>');
+        res.write('<html><body><h1>This is the home page</h1><a href="/students/">lien student</a> <a href="/student/">lien invalide</a></body></html>');
         res.end();
     } else if (req.url === '/students/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write('<html><body><h1>This is the student page</h1><a href="/">home</a></body></html>');
-        res.end(); 
+        res.end();
+    } else {
+        res.end('Invalid Request !');
     }
 });
 server.listen(5000);
