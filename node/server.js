0e3a15e87bdb00f68dc9afa9e296c67824740f80
Kai Carver <kaicarver@gmail.com>
2023-11-22 12:15:32 +0100

    premiere reponse HTML

--- node/server.js
+++ node/server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var server = http.createServer(function (req, res) {
-  //res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Bonjour monde !\n');
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html><body><p>This is home page</p></body></html>');
+        res.end();
+    }
 });
 server.listen(5000);
 console.log('Le serveur écoute sur le port 5000 http://localhost:5000/');
