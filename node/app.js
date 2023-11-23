97fa944613eb484b853501e66837bc956c17f84d
Kai Carver <kaicarver@gmail.com>
2023-11-23 15:58:34 +0100

    avec Formulaire

--- node/app.js
+++ node/app.js
@@ -11,24 +11,28 @@ var mongoose = require('mongoose');
 const url = "mongodb+srv://kaicarver:Stwotk9dbieqH9xt@cluster0.2rnu6yz.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(url)
-.then(console.log('Mongodb est connecté'))
-.catch(err => console.log(err));
+    .then(console.log('Mongodb est connecté'))
+    .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
 
 // models
-var instanceContact = require('./models/Contact'); // majuscule car c'est un Modèle
+var modContact = require('./models/Contact'); // majuscule car c'est un Modèle
 
 app.get('/', function (req, res) {
-    instanceContact.find().then(data => {
+    modContact.find().then(data => {
         console.log(data);
         res.render('Home', {data: data});
     }).catch(err => console.log(err));
 });
 
+app.get('/formulaire', function (req, res) {
+    res.render('Formulaire');
+});
+
 app.post('/submit-form-data', function (req, res) {
     console.log("Prénom :", req.body.firstname);
-    const Data = new instanceContact({
+    const Data = new modContact({
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         email: req.body.email,
