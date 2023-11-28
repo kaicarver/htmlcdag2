389f4a8cba6318ab9509474f07783467658a5ce7
Kai Carver <kaicarver@gmail.com>
2023-11-28 20:27:38 +0100

    bouton qui change le message dans le state du composant

--- react/my-app/src/components/Message.js
+++ react/my-app/src/components/Message.js
@@ -8,7 +8,16 @@ class Message extends Component {
         };
     }
     render() {
-        return <h1>{this.state.message}</h1>
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={() =>
+                    this.setState({
+                        message: 'Welcome visitor from button'
+                    })
+                }>Click me</button>
+            </div>
+        )
     } 
 };
 
