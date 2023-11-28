6460cf9897c2b438df5b9a4171e7d910e8899ddc
Kai Carver <kaicarver@gmail.com>
2023-11-28 14:35:14 +0100

    1er composant React

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Hello React !</h1>
+    <Greet></Greet>
     </div>
   );
 }
