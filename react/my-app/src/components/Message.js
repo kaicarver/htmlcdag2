b3083624cad5ae62839504f047c9c0fc8ce9dd06
Kai Carver <kaicarver@gmail.com>
2023-11-28 20:15:52 +0100

    message dans le state du composant

--- react/my-app/src/components/Message.js
+++ react/my-app/src/components/Message.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: 'Welcome visitorzz from state'
+        };
+    }
     render() {
-        return (
-            <div>
-                <h1>Hello Visitorzz</h1>
-            </div>
-        );
+        return <h1>{this.state.message}</h1>
     } 
 };
 
