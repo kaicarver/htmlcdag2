7152ba991cb3c47bfd9fe60df005355f5d2e2dda
Kai Carver <kaicarver@gmail.com>
2023-11-29 14:21:46 +0100

    avec du JSON

--- react/my-app/src/components/NameList.js
+++ react/my-app/src/components/NameList.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['Tom', 'Max', 'Ké'];
+    // const names = ['Tom', 'Max', 'Ké'];
+    const persons = [
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Clark',
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Diana',
+            age: 28,
+            skill: 'Vue'
+        }
+    ];
+    let personList = persons.map(person => (
+    <h2>
+        Je suis {person.name} j'ai {person.age} ans je connais {person.skill}
+    </h2>))
     return (
         <div>
-            { names.map((name, index) => <h2>map {index}: {name}</h2>)}
+            {personList}
         </div>
     )
 }
