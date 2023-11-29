ce814341b7766c52fbe9491fde85c88dd1cee6f5
Kai Carver <kaicarver@gmail.com>
2023-11-29 12:41:42 +0100

    solution bad

--- react/my-app/src/components/UserGreeting.js
+++ react/my-app/src/components/UserGreeting.js
@@ -4,14 +4,22 @@ class UserGreeting extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoggedIn: true,
+            isLoggedIn: false,
         }
     }
+    clickHandler() {
+        this.setState({
+            isLoggedIn: true
+        })
+        console.log(this.isLoggedIn);
+    };
     render() {
             if (this.state.isLoggedIn) {
                 return <div>Hello logged in</div>
             } else {
-                return <div>Hello Guest</div>
+                return <div>Hello Guest
+                    <button onClick={() => this.clickHandler()}>Connect</button>
+                    </div>
             }
     }
 }
