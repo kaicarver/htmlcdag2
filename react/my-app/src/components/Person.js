0b6437d90af4e419a29a2d84ee9fc3ca8a71ac68
Kai Carver <kaicarver@gmail.com>
2023-11-30 10:20:22 +0100

    exo stylesheet Person

--- react/my-app/src/components/Person.js
+++ react/my-app/src/components/Person.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './person.css'
 
 function Person({person}) {
+    var special = person.name === 'Diana' ? 'diana' : ''
     return (
         <div>
-            <h2>
+            <h2 className={special}>
                 Je suis {person.name} j'ai {person.age} ans je connais {person.skill} !
             </h2>
         </div>
