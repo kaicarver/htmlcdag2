23531b28b9be54580cd17488b02f8c233da804c4
Kai Carver <kaicarver@gmail.com>
2023-12-06 14:44:14 +0100

    afficher les infos JWT dans le front UserPage

--- react/my-app/src/components/User/UserPage.js
+++ react/my-app/src/components/User/UserPage.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function UserPage() {
     const [jwt, setJWT] = useState({});
     useEffect(() => {
-        axios.get('http://localhost:5000/getJWT', {withCredentials: true})
-    .then(response => {
-        setJWT(response.data)
-        console.log(response.data)
-    })
-    .catch(err => {
-        setJWT('')
-    })}, [])
+        axios.get('http://localhost:5000/getJWT', { withCredentials: true })
+            .then(response => {
+                setJWT(response.data)
+                console.log(response.data)
+            })
+            .catch(err => {
+                setJWT('')
+            })
+    }, [])
     return (
         <div>
-        UserPage
+            UserPage
+            <br />
+            user name = {jwt.username}
+            <br />
+            id = {jwt.id}
+            <br />
+            {jwt.admin ? "welcome admin" : "welcome user"}
 
         </div>
     )
