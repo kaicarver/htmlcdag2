9f3c6f8fdc141b2f5704837a7f4c1d776280ffcd
Kai Carver <kaicarver@gmail.com>
2023-12-20 12:55:00 +0100

    correction bug de modif sur photo; htmlspecialchars; autres petites choses

--- www/projetPHPg2/gestionproduits/edit.php
+++ www/projetPHPg2/gestionproduits/edit.php
@@ -1,14 +1,32 @@
 <?php
 if (isset($_POST['Ajouter']) || isset($_POST['Modifier'])) {
     require_once "connect.php";
-    $sql = isset($_POST['Ajouter']) ?
-        "insert into produits (designation, prix, photo) values (?, ?, ?)" :
-        "update produits set designation=?, prix=?, photo=? where id = ?";
+    $nomphoto = $_FILES['photo']['name'];
+    $tmpphoto = $_FILES['photo']['tmp_name'];
+    if (isset($_POST['Ajouter'])) {
+        $sql = "insert into produits (designation, prix, photo) values (?, ?, ?)";
+    } else {
+        if ($nomphoto) {
+            $sql = "update produits set designation=?, prix=?, photo=? where id = ?";
+        } else {
+            $sql = "update produits set designation=?, prix=? where id = ?";
+        }
+    }
     $ps = $pdo->prepare($sql);
-    $sql = isset($_POST['Ajouter']) ?
-        $ps->execute([$_POST['designation'], $_POST['prix'], $_POST['photo']]) :
-        $ps->execute([$_POST['designation'], $_POST['prix'], $_POST['photo'], $_POST['id']]);
+    if (isset($_POST['Ajouter'])) {
+        $ps->execute([$_POST['designation'], $_POST['prix'], $nomphoto]);
+    } else {
+        if ($nomphoto) {
+            $ps->execute([$_POST['designation'], $_POST['prix'], $nomphoto, $_POST['id']]);
+        } else {
+            $ps->execute([$_POST['designation'], $_POST['prix'], $_POST['id']]);
+        }
+    }
+    if ($nomphoto) {
+        move_uploaded_file($tmpphoto, "./img/".$nomphoto);
+    }
     header("location: list.php");
+    // echo "designation=",$_POST['designation'], "prix=",$_POST['prix'], "nomphoto=",$nomphoto, "id=",$_POST['id'];
 }
 if (isset($_GET['id'])) {
     $libaction = "Modifier";
@@ -23,8 +41,8 @@ if (isset($_GET['id'])) {
 } else {
     $libaction = "Ajouter";
     $id = "";
-    $designation = "";
-    $prix = "";
+    $designation = "Produit 42";
+    $prix = "42.42";
     $photo = "";
 }
 ?>
@@ -34,15 +52,17 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $libaction ?> Produit</title>
+    <style>img:hover { transform: scale(20); }</style>
 </head>
 <body>
     <?php require_once "header.php"; ?>
     <h1><?php echo $libaction ?> Produit</h1>
-    <form action="edit.php" method="post">
+    <form enctype="multipart/form-data" action="edit.php" method="post">
         <input type="hidden" name="id" value="<?php echo $id ?>">
         <label for="designation">Désignation</label><input type="text" name="designation" id="designation" value="<?php echo $designation ?>">
         <label for="prix">Prix</label><input type="text" name="prix" id="prix" value="<?php echo $prix ?>">
-        <label for="photo">Photo</label><input type="text" name="photo" id="photo" value="<?php echo $photo ?>">
+        <label for="photo">Photo</label><input type="file" name="photo" id="photo">
+        <img src="img/<?php echo $photo ?>" alt="<?php echo $photo ?>" title="<?php echo $photo ?>" style="height: 20px">
         <input type="submit" value="<?php echo $libaction ?>" name="<?php echo $libaction ?>">
     </form>
 </body>
--- www/projetPHPg2/gestionproduits/list.php
+++ www/projetPHPg2/gestionproduits/list.php
@@ -8,13 +8,30 @@ $ps->execute();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Liste Produits</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>img:hover { transform: scale(5); }</style>
 </head>
 <body>
     <?php require_once "header.php"; ?>
     <h1>Liste Produits</h1>
-    <?php while ($produit = $ps->fetch()) { ?>
-        <div><?php echo $produit["id"] ?> <?php echo $produit["designation"] ?> <?php echo $produit["prix"] ?> <?php echo $produit["photo"] ?> <a href="edit.php?id=<?php echo $produit["id"] ?>">Modifier</a> <a href="delete.php?id=<?php echo $produit["id"] ?>">Supprimer</a></div>
-    <?php } ?>
+    <div>
+        <table class="table">
+            <?php while ($produit = $ps->fetch()) { ?>
+            <tr>
+                <td><?php echo $produit["id"] ?></td>
+                <td><?php echo htmlspecialchars($produit["designation"]) ?></td>
+                <td><?php echo $produit["prix"] ?></td>
+                <td>
+                    <img src="img/<?php echo $produit["photo"] ?>" alt="<?php echo $produit["photo"] ?>" title="<?php echo $produit["photo"] ?>" style="height: 40px; hover{transform: scale(1.1);}" />
+                </td>
+                <td><a href="edit.php?id=<?php echo $produit["id"] ?>">Modifier <i class="bi bi-pencil-square"></i></a></td>
+                <td><a href="delete.php?id=<?php echo $produit["id"] ?>">Supprimer <i class="bi bi-trash"></i></a></td>
+            </tr>
+            <?php } ?>
+        </table>
+    </div>
+
 </body>
 </html>
