780346fc44a1135c63f2a532f054f19574b4c100
Kai Carver <kaicarver@gmail.com>
2023-12-21 15:33:21 +0100

    afficher login si loggé

--- www/projetPHPg2/shop/header.php
+++ www/projetPHPg2/shop/header.php
@@ -3,4 +3,5 @@
     <a href="newclient.php">Nouveau Client (user)</a>
     <a href="login.php">Login</a>
     <a href="logout.php">Logout</a>
+<?php echo isset($_SESSION['PROFILE']) ? $_SESSION['PROFILE']['login'] : "pas loggé"; ?>
 </div>
