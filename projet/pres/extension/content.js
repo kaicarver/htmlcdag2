d81803662b391fccab0019770ab7d3583e4fe920
Kai Carver <kaicarver@gmail.com>
2024-12-22 21:30:19 +0100

    add Time Inserter extension

---
+++ projet/pres/extension/content.js
@@ -0,0 +1,14 @@
+document.addEventListener('input', function(event) {
+  const activeElement = document.activeElement;
+  if (activeElement && (activeElement.tagName === 'TEXTAREA' || (activeElement.tagName === 'INPUT' && activeElement.type === 'text'))) {
+    const value = activeElement.value;
+    const cursorPosition = activeElement.selectionStart;
+    const trigger = ':t ';
+    if (value.substring(cursorPosition - trigger.length, cursorPosition) === trigger) {
+      const time = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+      const newValue = value.substring(0, cursorPosition - trigger.length) + time + value.substring(cursorPosition);
+      activeElement.value = newValue;
+      activeElement.selectionStart = activeElement.selectionEnd = cursorPosition - trigger.length + time.length;
+    }
+  }
+});
