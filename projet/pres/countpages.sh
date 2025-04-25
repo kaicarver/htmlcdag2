e4b467118321419f2dc88cf913ecdc8b6a432248
Kai Carver <kaicarver@gmail.com>
2025-04-25 17:27:22 +0200

    intégrer comptage de pages

--- projet/pres/countpages.sh
+++ projet/pres/countpages.sh
@@ -1,2 +1,6 @@
 # maybe a way to count pages in the PDF?
+# doesn't show much, but over time I may be able to deduce something
+# using a command like:
+#   ./countpages.sh > countpages.17.out
+# and comparing different outputs over time
 strings dossier-projet.pdf | sort | uniq -c | sort -nr | head
