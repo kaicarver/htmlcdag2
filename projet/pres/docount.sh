23096630ac0ba5d95d5ce427e745053eaf906115
Kai Carver <kaicarver@gmail.com>
2025-06-09 12:54:15 -0400

    meilleur comptage de pages

--- projet/pres/docount.sh
+++ projet/pres/docount.sh
@@ -1,1 +1,1 @@
-grep FlateDecode countpages.`date +"%Y-%m-%d-%I-%p"`.out | perl -ane 'print "\n ---> Longueur estimée du mémoire : ", $F[0] - 30, " pages <---\n\n"'
+grep FlateDecode countpages.latest.*.out | perl -ane 'print "\n ---> Longueur estimée du mémoire : ", $F[0] - 30, " pages <---\n\n"'
